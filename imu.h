4ef526027b31d056bae27c0803025790791453c1
MichaelD33 <codeing33@yahoo.com>
2020-04-26 13:37:50 -0400

    removed unused function definitions

--- imu.h
+++ imu.h
@@ -5,28 +5,9 @@
 #include "config.h"
 #include "MedianFilter.h"
 
-#ifdef MPU6050_68
-  #define MPU_ADDR 0x68
-#endif
-
-#ifdef MPU6050_69
-  #define MPU_ADDR 0x69
-#endif
-
-axis_int16_t imu_rates();
-axis_float_t imu_angles();
-axis_float_t imu_gyro_angles();
-axis_float_t imu_accel_filtered();
-axis_float_t imu_accelG();
-float accel_roll();
-float accel_pitch();
-float benchmark();
 void initIMU();
 void readIMU();
-void processGyro();
-void processAcc();
-void imuCombine();
 
+axis_float_t imu_angles();
 
 #endif
-
